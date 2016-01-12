f1a2ed6094ec0c7c11578b0eb4bd4765a6d4eb59
Andrij Tkachenko <andreuka@i.ua>
2016-01-12 02:03:29 +0200

    create database on activation

--- buddypress-block-user.php
+++ buddypress-block-user.php
@@ -1,6 +1,35 @@
 <?php
-class BPBlockUser{
-    function __construct(){
-        echo 'test';
-    }
+/*
+    Plugin Name: BuddyPress Block User
+    Plugin URI: https://www.upwork.com/freelancers/~0170bd24e40bc723c6
+    Description: Allows users to block another users.
+    Author: Andrij Tkachenko
+    Version: 0.1
+    Author URI: https://www.upwork.com/freelancers/~0170bd24e40bc723c6
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
 }
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-BPBlockUser-activator.php
+ */
+function activate_bp_block_user() {
+    $plugin_dir = plugin_dir_path( __FILE__ );
+
+    require_once $plugin_dir . 'includes/class-BPBlockUser-activator.php';
+    BP_Block_User_Activator::activate();
+}
+
+register_activation_hook( __FILE__, 'activate_bp_block_user' );
+
+/**
+ * The code that runs during plugin deactivation.
+function deactivate_wp_habit_builder() {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/class-WPHabbitBuilder-deactivator.php';
+    WP_Habbit_Builder_Deactivator::deactivate();
+}
+ */
