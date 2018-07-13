96c4a80afb10f8a3d50d82f55346b0c2c19a97cd
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-07-13 00:48:28 -0400

    Update class-BPBlockUser-activator.php

--- includes/class-BPBlockUser-activator.php
+++ includes/class-BPBlockUser-activator.php
@@ -1,22 +1,16 @@
 <?php
-/**
- * Activatin plugin
- */
-class BP_Block_User_Activator{
-
-    public static function activate(){
-        global $wpdb;
-
-        $query = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}bp_block_user` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `user_id` int(11) NOT NULL,
-          `blocked_id` int(11) NOT NULL,
-          PRIMARY KEY (`id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
-
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($query);
-
-    }
-
+/* Activatin plugin */
+class BP_Block_User_Activator {
+	public static function activate() {
+		global $wpdb;
+		$query = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}bp_block_user` (
+		`id` int(11) NOT NULL AUTO_INCREMENT,
+		`user_id` int(11) NOT NULL,
+		`blocked_id` int(11) NOT NULL,
+		PRIMARY KEY (`id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
+		
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($query);
+	}
 }
